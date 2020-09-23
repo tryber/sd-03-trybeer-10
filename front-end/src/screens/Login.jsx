a14979c040817732043c40f084b2780f1a8050f2
luis.campos <gucampos845@gmail.com>
2020-09-23 16:09:43 -0300

    Requisito 1

--- front-end/src/screens/Login.jsx
+++ front-end/src/screens/Login.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { Link, Redirect, useHistory} from 'react-router-dom';
+import { Link, Redirect, useHistory } from 'react-router-dom';
 
 const emailTest = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
 
-function Login (props) {
+function Login(props) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -17,31 +17,32 @@ function Login (props) {
 
   return (
     <div>
-      {email}
       <form id='login-form' method='POST'>
+        <label htmlFor="email">Email
+          <input
+            data-testid="email-input"
+            id="email"
+            type="email"
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </label>
+        <label htmlFor="password">Password
         <input
-          data-testid="email-input"
-          name="email"
-          type="email"
-          placeholder="Email"
-          onChange={(event) => setEmail(event.target.value)}
-        />
-        <input
-          data-testid="password-input"
-          name="password"
-          type="password"
-          placeholder="Password"
-          onChange={(event) => setPassword(event.target.value)}
-        />
+            data-testid="password-input"
+            id="password"
+            type="password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
         <button
           data-testid="signin-btn"
           type="button"
           onClick={async () => {
             // const form = new FormData (document.getElementById('login-form'));
-            const body = {"email":email, "password":password}
+            const body = { "email": email, "password": password }
             const headers = new Headers({
-                "Content-Type": "application/json",
-                "Content-Length": JSON.stringify(body).length
+              "Content-Type": "application/json",
+              "Content-Length": JSON.stringify(body).length
             })
             const user = await
               fetch('http://localhost:3001/login', { method: 'POST', body: JSON.stringify(body), headers })
@@ -49,16 +50,16 @@ function Login (props) {
                   .then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))));;
             setLocalStorage(user);
             if (user.role === 'administrator') {
-              return props.history.push('/admin/home');
+              return props.history.push('/admin/orders');
             }
             return props.history.push('/products');
           }}
           disabled={!validateLogin()}
         >
-          Entrar
+          ENTRAR
         </button>
         <Link to='/register'>
-          <button>Ainda não tenho conta</button>
+          <button data-testId="no-account-btn">Ainda não tenho conta</button>
         </Link>
       </form>
     </div>
