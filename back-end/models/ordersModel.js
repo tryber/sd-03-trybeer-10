03b2e11b03cd73ae46b3350a779fd55fef4a0c3d
luis.campos <gucampos845@gmail.com>
2020-09-28 15:46:13 -0300

    Tela de Pedidos

---
+++ back-end/models/ordersModel.js
@@ -0,0 +1,16 @@
+const connection = require('./connection');
+
+const fetchOrders = async () => {
+  const db = await connection();
+
+  const fetch = await db.getTable('sales').select(['id', 'total_price', 'delivery_number', 'sale_date'])
+    .execute();
+
+  const recipes = fetch.fetchAll();
+
+  return recipes.map(([id, total_price, delivery_number, sale_date]) => ({ id, total_price, delivery_number, sale_date }));
+};
+
+module.exports = {
+  fetchOrders,
+};
