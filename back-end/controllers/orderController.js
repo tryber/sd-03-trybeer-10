4d3cf722790f243d174ae604de35adc0d1144964
mirellaalves <mirella.alves@gmail.com>
2020-09-30 20:39:54 -0300

    go on order detail page and adjust order detail controller

--- back-end/controllers/orderController.js
+++ back-end/controllers/orderController.js
@@ -21,7 +21,7 @@ const getOrderDetail = async (req, res) => {
   const { id } = req.params;
   const orderDetail = await orderService.getOrderDetail(id);
   const orderById = await orderService.getOrderById(id);
-  return res.status(200).json(orderDetail, orderById);
+  return res.status(200).json({orderDetail, orderById});
 };
 
 module.exports = {
--- front-end/src/screens/OrderDetail.jsx
+++ front-end/src/screens/OrderDetail.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
 import Header from '../components/Header';
 
 function OrderDetail(props) {
   const [loggedIn, setLoggedIn] = useState(true);
   const [orderDetail, setOrderDetail] = useState([]);
+  const currentUser = JSON.parse(localStorage.getItem('user'));
 
   useEffect(() => {
     if (!currentUser) return props.history.push('/login');
@@ -11,7 +13,7 @@ function OrderDetail(props) {
       "Authorization": currentUser.token
     });
 
-    fetch(`http://localhost:3001/orders/${saleid}`, { headers })
+    fetch(`http://localhost:3001/orders/${orderDetail.saleid}`, { headers })
       .then((response) => response.json()
         .then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))))
       .then((data) => setOrderDetail(data))
@@ -20,32 +22,34 @@ function OrderDetail(props) {
 
   if(!loggedIn) return <Redirect to="/login"/>
 
-  const setTime = (date) => {
-    const format = new Date(date);
-    format.setHours(format.getHours() + 3)
-    return format;
-  }
+  // const setTime = (date) => {
+  //   const format = new Date(date);
+  //   format.setHours(format.getHours() + 3)
+  //   return format;
+  // }
 
-  const dateFormat = { day: '2-digit', month: '2-digit' }
+  // const dateFormat = { day: '2-digit', month: '2-digit' }
   
   return (
     <div>
       <Header title="Detalhes de Pedido"/>
-      <h2 data-testid="order-number">Pedido no.</h2>
-      <h2 data-testid="order-date">Data</h2>
+      <h2 data-testid="order-number">Pedido {orderDetail.id}</h2>
+      <h2 data-testid="order-date">{orderDetail.saleDate}</h2>
+        {orderDetail.map(({ quantity, name, price }, index) =>
       <table>
-        {mapProductData(data)}
         <tr>
-          <td data-testid="0-product-qtd">Qtde</td>
+          <td data-testid={`${index}-product-qtd`}>{quantity}</td>
         </tr>
         <tr>
-          <td data-testid="0-product-name">Nome do produto</td>
+          <td data-testid={`${index}-product-name`}>{name}</td>
         </tr>
         <tr>
-          <td data-testid="0-product-total-value">Valor total do produto</td>
+          <td data-testid={`${index}-product-total-value`}>{price*quantity}</td>
         </tr>
       </table>
-      <h3 data-testid="order-total-value">Total do pedido</h3>
+        )
+      }
+      <h3 data-testid="order-total-value">Total {orderDetail.totalPrice}</h3>
     </div>
   )
 }
